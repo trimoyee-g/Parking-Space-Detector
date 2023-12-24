924b53e22534d46eb8d25f3b0a2fd0d88e7b4280
trimoyee-g <trimoyeeg@gmail.com>
2023-12-24 20:04:29 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import cv2
+import pickle
+import cvzone
+import numpy as np
+
+# Video feed from the default camera (0)
+cap = cv2.VideoCapture(0)
+
+with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+
+width, height = 107, 60
+
+def checkParkingSpace(imgPro):
+    spaceCounter = 0
+
+    for pos in posList:
+        x, y = pos
+
+        imgCrop = imgPro[y:y + height, x:x + width]
+        count = cv2.countNonZero(imgCrop)
+
+        if count < 900:
+            color = (0, 255, 0)
+            thickness = 5
+            spaceCounter += 1
+        else:
+            color = (0, 0, 255)
+            thickness = 2
+
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, thickness)
+        cvzone.putTextRect(img, str(count), (x, y + height - 3), scale=1,
+                           thickness=2, offset=0, colorR=color)
+
+    cvzone.putTextRect(img, f'Free: {spaceCounter}/{len(posList)}', (100, 50), scale=3,
+                           thickness=5, offset=20, colorR=(0, 200, 0))
+
+while True:
+    # Capture frame-by-frame
+    ret, img = cap.read()
+    
+    # Perform image processing
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                         cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+    # Analyze parking spaces
+    checkParkingSpace(imgDilate)
+
+    # Display the frame
+    cv2.imshow("Parking Analysis", img)
+    
+    # Break the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close windows
+cap.release()
+cv2.destroyAllWindows()
